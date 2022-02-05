b446bcb9ed48c9b0080b48623e3a0d49e7a4c686
NicMotta <34142261+NicMotta@users.noreply.github.com>
2022-02-05 18:09:52 -0300

    artstation

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ window.onload = function () {
   <!-- HEADER -->`;
 
   document.getElementById("footer").innerHTML = `
-  <div class="grid grid-cols-2 gap-1 text-sm text-center mt-3 m-1 md:grid-cols-4 text-gray-500">
+  <div class="grid grid-cols-2 gap-1 text-sm text-center mt-3 m-1 md:grid-cols-5 text-gray-500">
   <a href="https://www.instagram.com/nicmotta_" class="border-2 border-gray-300 hover:bg-gray-300">instagram</a>
   <a href="https://www.linkedin.com/in/nic-motta/" class="border-2 border-gray-300 hover:bg-gray-300">linkedin</a>
   <a href="https://www.youtube.com/channel/UCzcHwERGhtBfxYfLtzDT0nA" class="border-2 border-gray-300 hover:bg-gray-300">youtube</a>
   <a href="https://github.com/NicMotta" class="border-2 border-gray-300 hover:bg-gray-300">github</a>
+  <a href="nicmotta.artstation.com" class="border-2 border-gray-300 hover:bg-gray-300">artstation</a>
   
 </div>
 <p class="text-center text-base text-gray-500">n i c , m o t t a  - 2022</p>
